39db0c1f6d44e76ce7da31120f457ac52406ac2b
justinhamilton125 <123605886+justinhamilton125@users.noreply.github.com>
2023-11-23 20:09:54 -0700

    Delete Project 1: Home Credit Default Model Files/GitHub Set Up Files directory

--- Project 1: Home Credit Default Model Files/GitHub Set Up Files/RScript Saved to Project.R
+++
@@ -1,6 +0,0 @@
-# Testing an RScript Put into Project into GitHub by RStdio Project
-
-
-# Added a second line to practice pushing.
-
-# Edited the Rscript in the new branch3-practice to practice merging branches
